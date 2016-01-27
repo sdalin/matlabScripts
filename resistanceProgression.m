f976ea4911d02ac511cbd9109b790b34365c070e
sdalin <sdalin@gmail.com>
2016-01-27 17:48:16 -0500

    "plot error for sharing with honi"

--- resistanceProgression.m
+++ resistanceProgression.m
@@ -117,10 +117,21 @@ end
 %Make a matrix with 4 columns, specifying rows of each drug which have rank
 %below 50 for the previous 3 weeks
 
+below50Lines = [];
 
 for drug = 1:4
     dummy = sum(squeeze(cellLineRankings(:,drug,:))<50,2);
-    selectedLines(:,drug) = find(dummy == 3);
+    temporary = find(dummy == 3);
+    
+    if length(temporary) > length(below50Lines)
+        numExtraRows = length(temporary) - length(below50Lines);
+        below50Lines = [below50Lines;nan(numExtraRows,drug-1)];
+    elseif length(temporary) < length(below50Lines)
+        numExtraRows = length(below50Lines) - length(temporary);
+        temporary = [temporary;nan(numExtraRows,1)];
+    end
+    
+    below50Lines = [below50Lines, temporary];
 end
 
 
@@ -134,46 +145,67 @@ end
 %sahdes of green, drug 4 sahdes of blue.
 
 
+greyco = zeros(7,3);
+greyco(:) = 0.8;
+
+normalco = [0    0.4470    0.7410
+    0.8500    0.3250    0.0980
+    0.9290    0.6940    0.1250
+    0.4940    0.1840    0.5560
+    0.4660    0.6740    0.1880
+    0.3010    0.7450    0.9330
+    0.6350    0.0780    0.1840];
+
 %First plot rank vs. week
 figure(1)
 x = 1:numWeeks;
 
 for plot = 1:4
     subplot(2,2,plot)
-        y1 = squeeze(cellLineRankings(:,plot,:));
-        y2 = squeeze(dose(1,plot,:));
-        
-        [hAx,hLine1,hLine2] = plotyy(x,y1,x,y2);
-        
-        xlabel('Week')
-        ylabel(hAx(1),'Rank') %left y-axis
-        formatSpec = '[%s] (nM)';
-        ylabel(hAx(2),sprintf(formatSpec,drugNames{plot})) %right y-axis
-        
-        axis(hAx(2),[0 numWeeks+1 0 ceil(max(y2))+1])
-        
-        hAx(1).YColor = [0 0 0];
-        hAx(2).YColor = [0.4431    0.0431    0.6000];
-        hAx(1).YLim = [0 100];
-        hAx(1).XLim = [0 numWeeks + 1];
-        hAx(2).YLim = [0 ceil(max(y2)+(max(y2)/10))];
-        hAx(2).XLim = [0 numWeeks + 1];
-        hAx(1).YTick = [0 20 40 60 80 100];
-        hAx(2).YTick = linspace(0,ceil(max(y2)+(max(y2)/10)),6);
-        
-        hLine2.LineStyle = '--';
-        hLine2.LineWidth = 2;
-        hLine2.Color = [0 0 0];
-        hLine2.Marker = 'o';
-        hLine2.MarkerFaceColor = [0.4431    0.0431    0.6000];
-        hLine2.MarkerEdgeColor = [0 0 0];
-        hLine2.MarkerSize = 5;
-        
-        hAx(1).ColorOrder = colorVec(:,:,plot);
-        
-        title(sprintf(drugNames{plot}));
-        
-        hold on
+
+    below50LinesNoNAN = below50Lines(~isnan(below50Lines(:,plot)),plot);
+    
+    y1 = squeeze(cellLineRankings(:,plot,:));
+    y2 = squeeze(dose(1,plot,:));
+    
+    set(groot,'defaultAxesColorOrder',greyco);
+    [hAx,hLine1,hLine2] = plotyy(x,y1,x,y2);
+    
+    hold on
+    
+    set(groot,'defaultAxesColorOrder',normalco);
+    y3 = squeeze(cellLineRankings(below50LinesNoNAN,plot,:));
+    
+    sAx = plot(x,y3);
+    
+    
+    xlabel('Week')
+    ylabel(hAx(1),'Rank') %left y-axis
+    formatSpec = '[%s] (nM)';
+    ylabel(hAx(2),sprintf(formatSpec,drugNames{plot})) %right y-axis
+
+    axis(hAx(2),[0 numWeeks+1 0 ceil(max(y2))+1])
+
+    hAx(1).YColor = [0 0 0];
+    hAx(2).YColor = [0.4431    0.0431    0.6000];
+    hAx(1).YLim = [0 100];
+    hAx(1).XLim = [0 numWeeks + 1];
+    hAx(2).YLim = [0 ceil(max(y2)+(max(y2)/10))];
+    hAx(2).XLim = [0 numWeeks + 1];
+    hAx(1).YTick = [0 20 40 60 80 100];
+    hAx(2).YTick = linspace(0,ceil(max(y2)+(max(y2)/10)),6);
+
+    hLine2.LineStyle = '--';
+    hLine2.LineWidth = 2;
+    hLine2.Color = [0 0 0];
+    hLine2.Marker = 'o';
+    hLine2.MarkerFaceColor = [0.4431    0.0431    0.6000];
+    hLine2.MarkerEdgeColor = [0 0 0];
+    hLine2.MarkerSize = 5;
+    
+    title(sprintf(drugNames{plot}));
+
+    hold on
 end
 
 
