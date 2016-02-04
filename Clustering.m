15cb7170e941f83b24c5a5338c10b42e16fa7dbc
sdalin <sdalin@gmail.com>
2016-02-04 16:26:26 -0500

    Added heirarcical clustering, PCA, other matlab clustering algorithms...

--- Clustering.m
+++ Clustering.m
@@ -2,19 +2,36 @@
 %Written by Simona Dalin
 %K-means and heierarchical clustering of log2 IC50 data
 %inputs: data (columns=drugs,rows=cell lines)
+%%
+%K-means Clustering
 
-% cidx = nan(10,1);
-% cmeans = nan(10,1);
-% sumd = nan(10,1);
-% 
-% for numClust=1:10
-%     [cidx(numClust),cmeans(numClust),sumd(numClust)] = kmeans(IC50data,numClust,'replicates',5);
-% end
+%Determine optimal number of clusters
+eva = evalclusters(IC50data,'kmeans','gap','KList',[1:32],'Distance','cityblock');
 
+distanceErrors = exp(eva.LogW).*eva.SE;
+
+figure(1)
+ax = errorbar(eva.InspectedK,exp(eva.LogW),distanceErrors,'k','LineWidth',2);
+grid on
+set(gca,'XTick',eva.InspectedK)
+xlim([0 32])
+xlabel('Number of Clusters')
+ylabel('Euclidian Distance Within Clusters')
+title('Evaluation of Number of Clusters, K-means, cityblock distance')
+
+idx9 = kmeans(IC50data,9);
+
+
+%%
+%Using linkage then cluster generates this:
+T = clusterdata(IC50data,1);
+
+%%
+%Heiraricical Clustering
 eucD = pdist(IC50data,'euclidean');
 clustTreeEuc = linkage(eucD,'average');
 
-cophenet(clustTreeEuc,eucD)
+cophenet(clustTreeEuc,eucD);
 
 [h,nodes] = dendrogram(clustTreeEuc,0);
 h_gca = gca;
@@ -23,3 +40,9 @@ h_gca.TickLength = [.002 0];
 h_gca.XTickLabel = [];
 
 CGobj = clustergram(IC50data,'RowLabels',RowLabels,'ColumnLabels',ColumnLabels,'Colormap',redbluecmap);
+
+%%
+%Some dimensional reduction to get a look at these suckers
+
+[wcoeff,score,latent,tsquared,explained] = pca(IC50data);
+
