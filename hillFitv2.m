0973fcf8341679ac8c4b88db29f050de3fe77a29
sdalin <sdalin@gmail.com>
2016-06-08 20:04:00 -0400

    "debugged"

--- hillFitv2.m
+++ hillFitv2.m
@@ -36,34 +36,35 @@ opts.Upper = [2 0.3 1000 Inf];
 %Run the actual fit for the normalized cell counts in bigstructNormed
 %create structure where each plate has its own field in which columns are
 %different cell lines (as on the original plate) and rows are A0,Ainf,IC50,n,rsquare,adjrsquare
-plates = char(fieldnames(bigstructNormed));
+plates = fieldnames(bigstructNormed);
+
 
 fittedStruct = struct;
-fittedHill = cell(length(bigstructNormed.(plates(1,:)))/16,4);
-gof = cell(length(bigstructNormed.(plates(1,:)))/16,2);
+fittedHill = cell(length(bigstructNormed.(char(plates(1,:))))/16,4);
+gof = cell(length(bigstructNormed.(char(plates(1,:))))/16,2);
 
 for k = 1:size(plates,1)
     %This next for loop does the fit for each column of the 384-well plate
-    for cellLine = 1:size(bigstructNormed.(plates(k,:)),2)
+    for cellLine = 1:size(bigstructNormed.(char(plates(k,:))),2)
         %Check if there are any NANs in the normalized cell counts we're
         %about to fit and if there are, remove that value as well as the
         %concentration that goes along with it;
-        thisCellLine = bigstructNormed.(plates(k,:))(1+(16*(cellLine-1)):(16+(16*(cellLine-1))));
-        thisCellLine(isnan(bigstructNormed.(plates(k,:))(1+(16*(cellLine-1)):(16+(16*(cellLine-1)))))) = [];
+        thisCellLine = bigstructNormed.(char(plates(k,:)))(1+(16*(cellLine-1)):(16+(16*(cellLine-1))));
+        thisCellLine(isnan(bigstructNormed.(char(plates(k,:)))(1+(16*(cellLine-1)):(16+(16*(cellLine-1)))))) = [];
         currentConcs = concentrations;
-        currentConcs(isnan(bigstructNormed.(plates(k,:))(1+(16*(cellLine-1)):(16+(16*(cellLine-1)))))) = [];
+        currentConcs(isnan(bigstructNormed.(char(plates(k,:)))(1+(16*(cellLine-1)):(16+(16*(cellLine-1)))))) = [];
         
         %Now do the fit on the values with NANs removed:
         [fittedHill{cellLine}, gof{cellLine}] = fit(currentConcs,thisCellLine, hill, opts );
         
-        fittedStruct.(plates(k,:)) = zeros(6,20);
+        fittedStruct.(char(plates(k,:))) = zeros(6,20);
         %Load desired variables into fittedStruct
-        fittedStruct.(plates(k,:))(1,cellLine) = fittedHill{cellLine}.A0;
-        fittedStruct.(plates(k,:))(2,cellLine) = fittedHill{cellLine}.Ainf;
-        fittedStruct.(plates(k,:))(3,cellLine) = fittedHill{cellLine}.IC50;
-        fittedStruct.(plates(k,:))(4,cellLine) = fittedHill{cellLine}.n;
-        fittedStruct.(plates(k,:))(5,cellLine) = gof{cellLine}.rsquare;
-        fittedStruct.(plates(k,:))(6,cellLine) = gof{cellLine}.adjrsquare;
+        fittedStruct.(char(plates(k,:)))(1,cellLine) = fittedHill{cellLine}.A0;
+        fittedStruct.(char(plates(k,:)))(2,cellLine) = fittedHill{cellLine}.Ainf;
+        fittedStruct.(char(plates(k,:)))(3,cellLine) = fittedHill{cellLine}.IC50;
+        fittedStruct.(char(plates(k,:)))(4,cellLine) = fittedHill{cellLine}.n;
+        fittedStruct.(char(plates(k,:)))(5,cellLine) = gof{cellLine}.rsquare;
+        fittedStruct.(char(plates(k,:)))(6,cellLine) = gof{cellLine}.adjrsquare;
             
     end
 end
@@ -75,18 +76,18 @@ end
 rowNames = {'A0';'Ainf';'IC50';'n';'rsquare';'adjrsquare'};
 cd('/Users/sdalin/Dropbox (MIT)/Biology PhD/2016/Hemann Lab/CR.CS/SSC Heterogeneity DRCs/matlabOutput')
 for k = 1:size(plates,1)
-    cellLineIC50cell = cell(6,length(fittedStruct.(plates(k,:))));
-    for cellLine = 1:length(fittedStruct.(plates(k,:)))/16
-        cellLineIC50cell{1,cellLine} = fittedStruct.(plates(k,:))(1,cellLine);
-        cellLineIC50cell{2,cellLine} = fittedStruct.(plates(k,:))(2,cellLine);
-        cellLineIC50cell{3,cellLine} = fittedStruct.(plates(k,:))(3,cellLine);
-        cellLineIC50cell{4,cellLine} = fittedStruct.(plates(k,:))(4,cellLine);
-        cellLineIC50cell{5,cellLine} = fittedStruct.(plates(k,:))(5,cellLine);
-        cellLineIC50cell{6,cellLine} = fittedStruct.(plates(k,:))(6,cellLine);
+    cellLineIC50cell = cell(6,length(fittedStruct.(char(plates(k,:)))));
+    for cellLine = 1:length(fittedStruct.(char(plates(k,:))))/16
+        cellLineIC50cell{1,cellLine} = fittedStruct.(char(plates(k,:)))(1,cellLine);
+        cellLineIC50cell{2,cellLine} = fittedStruct.(char(plates(k,:)))(2,cellLine);
+        cellLineIC50cell{3,cellLine} = fittedStruct.(char(plates(k,:)))(3,cellLine);
+        cellLineIC50cell{4,cellLine} = fittedStruct.(char(plates(k,:)))(4,cellLine);
+        cellLineIC50cell{5,cellLine} = fittedStruct.(char(plates(k,:)))(5,cellLine);
+        cellLineIC50cell{6,cellLine} = fittedStruct.(char(plates(k,:)))(6,cellLine);
     end
     cellLineIC50cell = [rowNames,cellLineIC50cell];
     T = cell2table(cellLineIC50cell);
-    writetable(T,sprintf('%s',plates(k,:),'.txt'));
+    writetable(T,sprintf('%s',char(plates(k,:)),'.txt'));
 end
 
 
