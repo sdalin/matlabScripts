db78c20d256a412c9fe4594d5a95e6d5ca654380
sdalin <sdalin@gmail.com>
2016-07-01 17:45:41 -0400

    "fixed a random bug"

--- loop.m
+++ loop.m
@@ -49,4 +49,4 @@ for sepStruct = 1:length(list)
     end
 end
     
-[fittedStruct,individualFittedStruct] = hillFitv2(bigstructNormed,concentrations);
+[fittedStruct,individualFittedStruct] = hillFitv2(concatinatedStruct,concentrations);
