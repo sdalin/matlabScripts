6baa2c216236c0b353127b315ccf66c56d624e45
sdalin <sdalin@gmail.com>
2017-11-30 10:47:51 -0500

    "Modified data-reading script to read doses and drugs from seperate tabs to accomodate more varied plate layouts"

--- 384WellPlateReaderDRCAnalysis/ReadNormPlateReaderHTSDRCData.m
+++ 384WellPlateReaderDRCAnalysis/ReadNormPlateReaderHTSDRCData.m
@@ -15,6 +15,7 @@
 
 function [bigstructNormed] = ReadNormPlateReaderDRCData(filename)
 
+%% Start by extracting the meta-data and raw-data from the excel files
     %Find number of sheets in the excel file 'filename'
     [status,sheets] = xlsfinfo(filename);
     numOfSheets = numel(sheets);
@@ -23,16 +24,23 @@ function [bigstructNormed] = ReadNormPlateReaderDRCData(filename)
     %and import the drug and concentration information into a field
     %called 'Info'
     for sheet = 1:numOfSheets
-        if strcmp(sheets{sheet},'Info')
-            %Grab the drugNames
-            [num,drugNames,raw] = xlsread(filename,'Info','B30:Y30');
+        if strcmp(sheets{sheet},'Dose')
             %Grab the doses
-            [doses] = xlsread(filename,'Info', 'B30:Y46');
-            bigstructNormed.Info.drugNames = drugNames;
+            [num,text,doses] = xlsread(filename,'Dose','B24:Y39');
+            
+            %Store doses in bigstructNormed.Info
             bigstructNormed.Info.doses = doses;
+            
+        elseif strcmp(sheets{sheet},'Drugs')
+            %Grab the drugNames
+            [num,drugNames,raw] = xlsread(filename,'Drugs','B24:Y39');
+            
+            %Store drug names in BigstructNormed.Info
+            bigstructNormed.Info.drugNames = drugNames;
+            
         else
             %Grab the raw data from this sheet
-            [num,text,cellRawFluorescence] = xlsread(filename, sprintf('%s',sheets{sheet}), 'B31:Y46');
+            [num,text,cellRawFluorescence] = xlsread(filename, sprintf('%s',sheets{sheet}), 'B24:Y39');
             removeOVERs = cellRawFluorescence;
             removeOVERs(find(strcmp(cellRawFluorescence,'OVER'))) = {NaN};    
             rawFluorescence = cell2mat(removeOVERs);
@@ -43,21 +51,28 @@ function [bigstructNormed] = ReadNormPlateReaderDRCData(filename)
             bigstruct.(sprintf('%s',char(cellLine))) = rawFluorescence;
         end
     end
-    
-    %For each plate, remove background fluorescence in each column by
-    %subtraction the median of column 23, which had no cells, store in bigstructNormed.
-    for cellLine = 1:size(fieldnames(bigstruct),1)
-        cellLines = fieldnames(bigstruct);
-        backgroundFluorescence = nanmedian(bigstruct.(sprintf('%s',cellLines{cellLine}))(:,23));
-        bigstructNoBackground.(sprintf('%s',cellLines{cellLine})) = bigstruct.(sprintf('%s',cellLines{cellLine}))-backgroundFluorescence;
-    end
 
-    %Normalize each column of each plate to average of columns 1,2 and 24
+%% Remove background fluorescence (no need with luminescence?  Or at least, there are no wells without cells!    
+%     %For each plate, remove background fluorescence in each column by
+%     %subtraction the median of column 23, which had no cells, store in bigstructNormed.
+%     for cellLine = 1:size(fieldnames(bigstruct),1)
+%         cellLines = fieldnames(bigstruct);
+%         backgroundFluorescence = nanmedian(bigstruct.(sprintf('%s',cellLines{cellLine}))(:,23));
+%         bigstructNoBackground.(sprintf('%s',cellLines{cellLine})) = bigstruct.(sprintf('%s',cellLines{cellLine}))-backgroundFluorescence;
+%     end
+
+%% Normalize viability to median of DMSO-only wells (columns 2 & 24)
+
+    %Normalize each column of each plate to average of columns 2 and 24
     %which had no drugs
+    bigstructNormed.CVs = {};
+    cellLines = fieldnames(bigstruct);
     for cellLine = 1:size(fieldnames(bigstruct),1)
-        noDrugs = [bigstructNoBackground.(sprintf('%s',cellLines{cellLine}))(:,1:2),bigstructNoBackground.(sprintf('%s',cellLines{cellLine}))(:,24)];
+        noDrugs = [bigstruct.(sprintf('%s',cellLines{cellLine}))(:,2),bigstruct.(sprintf('%s',cellLines{cellLine}))(:,24)];
         medianNoDrug = nanmedian(noDrugs(:));
-        bigstructNormed.(sprintf('%s',cellLines{cellLine})) = bigstructNoBackground.(sprintf('%s',cellLines{cellLine}))/medianNoDrug;
+        CV = std(noDrugs(:))/mean(noDrugs(:));
+        bigstructNormed.(sprintf('%s',cellLines{cellLine})) = bigstruct.(sprintf('%s',cellLines{cellLine}))/medianNoDrug;
+        bigstructNormed.CVs(:,end+1) = {cellLines{cellLine};CV};
     end
     
 end
